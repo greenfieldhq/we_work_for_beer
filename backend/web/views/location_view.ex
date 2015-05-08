43a4376ad64695de91a8a3849db5c844097d38ab
faizaanshamsi <faizaanshamsi@gmail.com>
2015-05-08 13:51:45 -0400

    1. Mirage can be toggled, namespace matches backend 2. Locations have a serializer (using Remodel)

--- backend/web/views/location_view.ex
+++ backend/web/views/location_view.ex
@@ -2,6 +2,6 @@ defmodule WeWorkForBeer.LocationView do
   use WeWorkForBeer.Web, :view
 
   def render("index.json", %{locations: locations}) do
-    locations  
+    LocationSerializer.to_map locations
   end
 end
