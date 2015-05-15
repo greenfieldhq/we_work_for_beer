64e67287b7c810dca8d68267e0d7f6f5a86c8823
Eric Kelly <heroiceric@gmail.com>
2015-05-15 11:38:14 -0400

    Update to ember-cli 0.2.4

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -1,12 +1,14 @@
 import Ember from 'ember';
 import config from './config/environment';
 
-var Router = Ember.Router.extend({
+const Router = Ember.Router.extend({
   location: config.locationType
 });
 
-export default Router.map(function() {
+Router.map(function() {
   this.route('locations', { path: '/' }, function() {
     this.route('location', { path: 'locations/:location_id' });
   });
 });
+
+export default Router;
