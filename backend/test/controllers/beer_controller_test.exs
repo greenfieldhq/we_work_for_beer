f48d949cc1669ab4fd5542e6d99bbbacff62b1e2
Ryan Tremaine <ryan.trem@gmail.com>
2015-07-31 14:39:19 -0400

    fix beer_controller tests

--- backend/test/controllers/beer_controller_test.exs
+++ backend/test/controllers/beer_controller_test.exs
@@ -7,7 +7,11 @@ defmodule WeWorkForBeer.BeerControllerTest do
     description: "gittin' drunk",
     name: "Drunk Guy Ale",
     style: "some content",
-    uid: "some content"
+    uid: "some content",
+    brewery_name: "Sofa King Good Beer",
+    icon_small: "small.png",
+    icon_medium: "medium.png",
+    icon_large: "large.png"
   }
   @invalid_attrs %{}
 
@@ -31,13 +35,21 @@ defmodule WeWorkForBeer.BeerControllerTest do
         "style" => beer1.style,
         "abv" => to_string(beer1.abv),
         "description" => beer1.description,
+        "brewery_name" => beer1.brewery_name,
+        "icon_small" => beer1.icon_small,
+        "icon_medium" => beer1.icon_medium,
+        "icon_large" => beer1.icon_large
       },
       %{
         "id" => beer2.id,
         "name" => beer2.name,
         "style" => beer2.style,
         "abv" => to_string(beer2.abv),
-        "description" => beer2.description
+        "description" => beer2.description,
+        "brewery_name" => beer2.brewery_name,
+        "icon_small" => beer2.icon_small,
+        "icon_medium" => beer2.icon_medium,
+        "icon_large" => beer2.icon_large
       }
     ]
   end
@@ -50,7 +62,11 @@ defmodule WeWorkForBeer.BeerControllerTest do
       "name" => beer.name,
       "style" => beer.style,
       "abv" => to_string(beer.abv),
-      "description" => beer.description
+      "description" => beer.description,
+      "brewery_name" => beer.brewery_name,
+      "icon_small" => beer.icon_small,
+      "icon_medium" => beer.icon_medium,
+      "icon_large" => beer.icon_large
     }
   end
 end
